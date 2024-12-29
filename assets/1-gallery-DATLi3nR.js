f4b7611b51f2073eef49cbeb5d3f384b3147f2a1
ankhod <120341777+ankhod@users.noreply.github.com>
2024-12-29 20:58:33 +0000

    Deploying to gh-pages from @ ankhod/goit-js-hw-09@5eb57d006ff728a1c6b0492905addd36aed3fcb3 🚀

---
+++ assets/1-gallery-DATLi3nR.js
@@ -0,0 +1,2 @@
+import{S as a}from"./vendor-B-WyX8ve.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery-DATLi3nR.js.map
--- index.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as a}from"./assets/vendor-CgTBfC_f.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
-//# sourceMappingURL=index.js.map
