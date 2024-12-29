9fb608cd245b995c3d0a01712747cea262183f0b
ankhod <120341777+ankhod@users.noreply.github.com>
2024-12-29 22:57:47 +0000

    Deploying to gh-pages from @ ankhod/goit-js-hw-09@75cd9998beaa688cd4d80c4584129a77ecdb83eb 🚀

--- assets/1-gallery-CAMY_XZS.js
+++ assets/1-gallery-CAMY_XZS.js
@@ -1,2 +1,2 @@
 import{S as a}from"./vendor-B-WyX8ve.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
-//# sourceMappingURL=1-gallery-DATLi3nR.js.map
+//# sourceMappingURL=1-gallery-CAMY_XZS.js.map
