db7376f1f38776b3b0c5caf034ecfe426b7aba8e
ankhod <120341777+ankhod@users.noreply.github.com>
2024-12-30 23:32:15 +0000

    Deploying to gh-pages from @ ankhod/goit-js-hw-09@55e140777b148b1f154d9cb44750807a0c9a638a 🚀

--- assets/1-gallery-BJFI0pZR.js
+++ assets/1-gallery-BJFI0pZR.js
@@ -1,2 +1,2 @@
 import{S as a}from"./vendor-B-WyX8ve.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
-//# sourceMappingURL=1-gallery-CAMY_XZS.js.map
+//# sourceMappingURL=1-gallery-BJFI0pZR.js.map
